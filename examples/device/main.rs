aa447aab1eb1ecb696b1cf6442f537b2ae7622ed
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-04-29 13:37:49 -0700

    print the name

--- examples/device/main.rs
+++ examples/device/main.rs
@@ -2,7 +2,8 @@ use iron_oxide::*;
 
 unsafe fn execute() {
     let devices = MTLCopyAllDevices();
-    let _device = devices.into_iter().find_map(|d| Some(d));
+    let device = devices.into_iter().find_map(|d| Some(d)).unwrap();
+    println!("Device name: {}", device.get_name());
 }
 
 fn main() {

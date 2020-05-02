ad828b4b14be24fddff1d0ddabb19d2b031e20e0
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-05-02 10:27:58 -0700

    storage mod

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ Unsafe Rusty [Metal](https://developer.apple.com/documentation/metal?language=ob
 
 Backfill List
 -
-- MTLDevice Mojave / Catalina behavior
-- Fence + event stuff
-- MTLFunctionConstantValues
-- MTLFunction input behavior
-- Preprocesser macros for library files
-- Asynchronous methods callously ditched for synchronous ones
-- Tile shaders / tile render pipeline
-- MTLVertexDescriptor
-- Buffer mutability checks
-- Render pipeline tessellation stage configuration
-- Render pipeline indirect command buffer + vertex amplification
-- Render pipeline state behavior
-- Compute pipeline state behavior
-- Compute pipeline descriptor
-- Heaps
+- [ ] MTLDevice Mojave / Catalina behavior
+- [ ] Fence + event stuff
+- [ ] MTLFunctionConstantValues
+- [ ] MTLFunction input behavior
+- [ ] Preprocesser macros for library files
+- [ ] Asynchronous methods callously ditched for synchronous ones
+- [ ] Tile shaders / tile render pipeline
+- [ ] MTLVertexDescriptor
+- [ ] Buffer mutability checks
+- [ ] Render pipeline tessellation stage configuration
+- [ ] Render pipeline indirect command buffer + vertex amplification
+- [ ] Render pipeline state behavior
+- [ ] Compute pipeline state behavior
+- [ ] Compute pipeline descriptor
+- [ ] Heaps
--- src/resource/mod.rs
+++ src/resource/mod.rs
@@ -7,7 +7,23 @@ use crate::import_objc_macros::*;
 /// Analogous to [this](https://developer.apple.com/documentation/metal/mtlcpucachemode?language=objc).
 pub enum MTLCPUCacheMode {
     Default = 0,
+    /// Optimized for writes by the CPU. Reads may be inefficient.
     WriteCombined = 1,
+}
+
+#[repr(u64)]
+/// Options for the memory location and access permissions for a resource.
+///
+/// Analogous to [this](https://developer.apple.com/documentation/metal/mtlstoragemode?language=objc).
+pub enum MTLStorageMode {
+    /// System memory is used. Both the GPU and CPU may access it.
+    Shared = 0,
+    /// The GPU and CPU maintain separate copies which must be explicitly synchronized. macOS only.
+    Managed = 1,
+    /// Only the GPU may access the resource.
+    Private = 2,
+    /// Only the GPU may access the resource and only during a single render pass. iOS only.
+    Memoryless = 3,
 }
 
 /// An allocation of GPU accessible memory. Implemented only on device created objects.
@@ -19,5 +35,10 @@ pub trait MTLResource: Object + DeviceCreated {
     /// property of the resource.
     unsafe fn set_cpu_cache_mode(&self, mode: MTLCPUCacheMode) {
         msg_send![self.get_ptr(), setCpuCacheMode:mode]
+    }
+    /// Sets the [storageMode](https://developer.apple.com/documentation/metal/mtlresource/1515477-storagemode?language=objc)
+    /// property of the resource.
+    unsafe fn set_storage_mode(&self, mode: MTLStorageMode) {
+        msg_send![self.get_ptr(), setStorageMode:mode]
     }
 }
